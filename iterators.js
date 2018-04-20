4aa4e03413793e082954bbad2e4320265c2e5b84
antalmega <antalmega@gmail.com>
2018-04-20 19:04:55 +0200

    updating iterators.js

--- iterators.js
+++ iterators.js
@@ -1,12 +1,41 @@
 // Write a function called listNames which takes in an array of songs and console.logs the name of each one.
 
+function listNames(songs) {
+    songs.forEach(song => console.log(song.name));
+}
+
 // Write a function called listSongDetails which takes in an array of songs and console.logs details about each one. The details should be in the following example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
+
+function listSongDetails(songs) {
+    songs.forEach(song => console.log(`${song.name}, by ${song.artist} (${song.duration})`));
+}
 
 // Write a function called summerJamCount which takes in an array of songs and returns the number of songs which hit #1 on the charts in June, July, or August.
 
+function summerJamCount(songs) {
+    let months = [6,7,8];
+    let counter = 0;
+    songs.forEach(song =>{
+        if (months.includes(song.month)) counter++
+    })
+    return counter;
+}
+
 // Write a function called getDurations which takes in an array of songs and returns an array of each song's duration.
 
+function getDurations(songs) {
+    return songs.map(song => song.duration);
+}
+
 // Write a function called getDurationInSeconds which takes in an array of songs and returns an array of each song's duration in seconds.
+
+function getDurationsInSeconds(songs) {
+    return songs.map(song => {
+        return song.duration.split(":")
+        .map((num, index) => index < 1 ? +num * 60 : +num)
+        .reduce((acc, next) => acc + next);
+    })
+}
 
 // Write a function called getMainArtists which takes in an array of songs and returns an array of the primary artists on the recordings. If there's only one artist, that artist should be returned; if there are featured artists, they should be ignored (so only the artist to the left of "featuring" is kept.)
 
