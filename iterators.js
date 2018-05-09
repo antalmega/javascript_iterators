f4009ff07a645e4efb4477e69c729a9f1a095488
antalmega <antalmega@gmail.com>
2018-05-09 20:44:19 +0200

    update iterators.js

--- iterators.js
+++ iterators.js
@@ -39,19 +39,67 @@ function getDurationsInSeconds(songs) {
 
 // Write a function called getMainArtists which takes in an array of songs and returns an array of the primary artists on the recordings. If there's only one artist, that artist should be returned; if there are featured artists, they should be ignored (so only the artist to the left of "featuring" is kept.)
 
+function getMainArtists(songs) {
+    return songs.map(song => {
+        return song.artist.split(' featuring')[0];
+    });
+}
+
 // Write a function called getBigHits which takes an array of songs and returns an array of songs which were number one for 10 or more weeks.
+
+function getBigHits(songs) {
+    return songs.filter(song => song.weeksAtNumberOne >= 10);
+}
 
 // Write a function called getShortSongs which takes an array of songs and returns an array of songs which shorter than 3 minutes.
 
+function getShortSongs(songs) {
+    return songs.filter(song => +song.duration.split(':')[0] < 3);
+}
+
 // Write a function called getSongsByArtist which takes in an array of artists and the name of an artist and returns an array of songs by that artist.
+
+function getSongsByArtist(songs, artist) {
+    return songs.filter(song => song.artist === artist);
+}
 
 // Refactor summerJamCount to use reduce!
 
+function summerJamCount(songs) {
+    let months = [6,7,8];
+    let counter = 0;
+    return songs.reduce((acc, song) => {
+        if (months.includes(song.month)) acc ++;
+        return acc;
+    }, 0);
+}
+
 // Write a function called getTotalDurationInSeconds which takes in an array of songs and returns the total amount of time (in seconds) it would take to listen to all of the songs. (Hint: can you use anything you've written already to help solve this problem?)
+
+function getTotalDurationInSeconds(songs) {
+    return getDurationsInSeconds(songs).reduce((total, songDuration) => {
+        return total + songDuration;
+    }, 0)
+}
 
 // Write a function called getSongCountByArtist which takes in an array of songs and returns an object. The keys in the object should be artist names, and the values should be the number of songs by that artist in the orignal array.
 
+function getSongCountByArtist(songs) {
+    return songs.reduce((obj, song) => {
+        let artist = song.artist;
+        artist in obj ? obj[artist]++ : obj[artist] = 1;
+        return obj;
+    }, {});
+}
+
 // Write a function called averageWeeksAtNumberOne which takes in an array of songs and returns the average number of weeks that songs on the list were #1.
+
+function averageWeeksAtNumberOne(songs) {
+    let sumWeeks = songs.reduce(weeks, song => {
+        return weeks + weekssong.weeksAtNumberOne;
+    });
+    return sumWeeks / songs.length;
+}
 
 
 
